16458380152818e898bb797b181473bc1c6b6860
eshoiza <engrjayt200@gmail.com>
2020-02-14 00:08:13 +0100

    updated components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,15 @@ import React from 'react';
 //import {Provider} from 'react-redux';
 //import {createStore} from 'redux';
 //import rootReducer from './';
-import Home from "../src/containers/Home";
-import About from "../src/containers/About";
-import Contact from "../src/containers/Contact";
 
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import logo from './jumia-logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <Switch>
-     <Route path="/" component={Home} exact/>
-    <Route path="/about" component={About}/>
-    <Route path="/contact-us" component={Contact}/>
-      
-    </Switch>
+   <div>
+     
+   </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+ 
     <App />
-  </BrowserRouter>,
+  ,
   document.getElementById("root")
 );
 
