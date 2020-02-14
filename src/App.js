fef3105f59e8d962cf77d4f40ad88b4a22d5ec6f
eshoiza <engrjayt200@gmail.com>
2020-02-14 01:24:43 +0100

    added reducer components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
-//import {Provider} from 'react-redux';
-//import {createStore} from 'redux';
-//import rootReducer from './';
+import React, { Component } from "react";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import rootReducer from './reducers';
+
+import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
 
 import logo from './jumia-logo.svg';
 import './App.css';
 
-function App() {
-  return (
-   <div>
-     
-   </div>
-  );
+import Home from "./pages/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import ProductDetail from "../src/pages/ProductDetail";
+import ShoppingCart from "../src/pages/ShoppingCart"; 
+
+export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+class App extends Component {
+  render() {
+    return (
+      <Provider store = {store}>
+<BrowserRouter>
+<React.Fragment>
+<Header />
+<Switch>  
+<Route exact path={"/"} render={() =>{
+  return <Redirect to={"/products"} />
+
+}}/>
+<Route exact path={'/products'} component={Home}/>
+<Route exact path={'/products/:id'} component={ProductDetail}/>
+<Route exact path={'/cart'} component={ShoppingCart} />
+
+</Switch>
+<Footer />
+</React.Fragment>
+
+</BrowserRouter>
+
+ </Provider>
+    );
+  }
 }
 
 export default App;
